c85652fdac9436d89b8ce9a588019a3ecbba90de
devdutt jangir <devdutt.jangir@3pillarglobal.com>
2021-07-27 17:18:46 +0530

    removed google api code

--- src/index.js
+++ src/index.js
@@ -3,38 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import { compose, withProps } from "recompose";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-
-const MyMapComponent = compose(
-  withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
-     */
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBFmxJTreirXd4LypPOecY9eBhGXz5FJuk&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} onMouseOver={() => alert("Hello")} />
-    )}
-  </GoogleMap>
-));
-
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
